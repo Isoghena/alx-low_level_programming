eab6b08a883d1f16df41d96df5cac88d4902acb1
Isoghena <estherewaia@yahoo.com>
2022-12-15 07:04:18 -0800

    draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*print_line - draw a straight line in the terminal
-*@n: number of times the character _ should be printed
-*/
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
 void print_line(int n)
 {
 	if (n <= 0)
@@ -13,10 +13,11 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i = 1, i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
+
 }
