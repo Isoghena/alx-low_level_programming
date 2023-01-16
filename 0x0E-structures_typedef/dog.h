f1afbf09d3d90a3c57e45c72ea13ca8f61a30df3
Isoghena <estherewaia@yahoo.com>
2023-01-16 07:16:35 -0800

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,17 @@
-#ifndef _DOG_H
-#define _DOG_H
-
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - initialize dog struct
+ * struct dog - dog structure
  *
- * @d: input pointer to struct dog
- * @name: input for dog name
- * @age: input for dog age
- * @owner: input for dog owner
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
  *
- * Return: nothing
 */
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d)
-	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
